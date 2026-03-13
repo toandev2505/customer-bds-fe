213f38a72d305ee1f5e46b37c064187747ff66d5
Toan Truong <toandev2505@gmail.com>
2026-03-13 16:46:03 +0700

    Fix merge conflict in index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
-=======
+
 import reportWebVitals from './reportWebVitals';
->>>>>>> 57d986a458d4e6ee57acfc2744278eb6a36eb29c
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -20,4 +19,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
->>>>>>> 57d986a458d4e6ee57acfc2744278eb6a36eb29c
